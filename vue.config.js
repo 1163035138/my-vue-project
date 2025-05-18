8be661888d1ead2d815a50edae4957f20901875d
zhangyadong <1163035138@qq.com>
2025-05-18 21:59:51 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   devServer: {
     proxy: {
-      '/api': {
+      "/api": {
         target: 'http://localhost:3000',
         changeOrigin: true,
         pathRewrite: {
