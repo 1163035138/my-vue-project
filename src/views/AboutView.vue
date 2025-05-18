945010e6659c969db211f8d88d8df885ac7a2001
zhangyadong <1163035138@qq.com>
2025-05-18 10:39:39 +0800

    feat:

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script setup>
+import axios from 'axios'
+import { onMounted } from 'vue'
+onMounted(() => {
+  axios.get('http://localhost:3000/user').then((res) => {
+    console.log(res)
+  })
+})
+</script>
